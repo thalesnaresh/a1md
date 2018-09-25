bb95e74974889758010400deb6ea32809a1ce651
kennythaleskkk <snaresh450@gmail.com>
2018-09-24 23:29:00 -0400

    first commit

---
+++ hw/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace hw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now.ToLongTimeString());
+            Console.WriteLine("hw app");
+
+            Console.ReadLine();
+        }
+    }
+}
